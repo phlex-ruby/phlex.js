38e41af607924b7e834faf45daa771b2da435f57
Joel Drapper <joel@drapper.me>
2024-03-04 22:09:01 +0000

    No more ayeframes

--- dist/ayeframe.js
+++ dist/ayeframe.js
@@ -1,34 +1,96 @@
 import { morph } from "morphlex";
-export function ayeFrame() {
-	createFrame("morph", (targetNode, content) => {
-		if (targetNode && content) morph(targetNode, content);
-	});
-	createFrame("replace", (targetNode, content) => {
-		if (targetNode && content) targetNode.replaceWith(content);
-	});
-	createFrame("append", (targetNode, content) => {
-		if (targetNode && content) targetNode.appendChild(content);
-	});
-	createFrame("prepend", (targetNode, content) => {
-		if (targetNode && content) targetNode.prepend(content);
-	});
-	createFrame("remove", (targetNode) => {
-		if (targetNode) targetNode.remove();
-	});
+const Actions = new Map();
+function error() {
+	throw new Error("Target or node not found");
 }
-function createFrame(name, action) {
-	for (const frame of document.querySelectorAll("iframe")) frame.name === name && frame.remove();
-	const frame = document.createElement("iframe");
-	frame.name = name;
-	frame.hidden = true;
-	frame.loading = "lazy";
-	frame.sandbox.add("allow-same-origin");
-	frame.addEventListener("load", () => {
-		const id = frame.contentWindow?.location?.hash;
-		if (id) {
-			action(document.querySelector(id), frame.contentDocument?.body?.firstChild);
+Actions.set("morph", ({ target, node }) => {
+	if (target && node) {
+		morph(target, node);
+	} else {
+		error();
+	}
+});
+Actions.set("replace", ({ target, node }) => {
+	if (target && node) {
+		target.replaceWith(node);
+	} else {
+		error();
+	}
+});
+Actions.set("append", ({ target, node }) => {
+	if (target && node) {
+		target.appendChild(node);
+	} else {
+		error();
+	}
+});
+Actions.set("prepend", ({ target, node }) => {
+	if (target && node) {
+		target.prepend(node);
+	} else {
+		error();
+	}
+});
+Actions.set("refresh", () => {
+	fetch(document.location.href).then((response) => {
+		if (response.ok) {
+			response.text().then((text) => {
+				morph(document.documentElement, text);
+			});
 		}
 	});
-	document.body.appendChild(frame);
+});
+Actions.set("remove", ({ target }) => {
+	target?.remove();
+});
+Actions.set("navigate", ({ node }) => {
+	if (node) morph(document.documentElement, node);
+});
+async function createEvent(href, targetId, fragment) {
+	const headers = new Headers();
+	if (fragment) headers.append("X-Fragment", fragment);
+	const event = await fetch(href, { headers }).then(async (response) => {
+		return await response.text().then((text) => {
+			const template = document.createElement("template");
+			template.innerHTML = text;
+			const node = template.content.firstElementChild;
+			const target = targetId ? document.getElementById(targetId) : null;
+			return {
+				target,
+				node,
+			};
+		});
+	});
+	return event;
+}
+export function ayeFrame() {
+	// document.addEventListener("submit", (event) => {
+	// 	if (event.target instanceof HTMLFormElement) {
+	// 		console.log("submitted a form");
+	// 	}
+	// });
+	document.addEventListener("click", (event) => {
+		const link = event.target;
+		if (link instanceof HTMLAnchorElement) {
+			if (event.metaKey || event.ctrlKey || event.shiftKey) return;
+			if (link.ariaDisabled) {
+				event.preventDefault();
+				return;
+			}
+			const href = link.href;
+			const action = link.dataset.action;
+			const targetId = link.dataset.target;
+			const fragment = link.dataset.fragment;
+			if (action) {
+				event.preventDefault();
+				link.ariaDisabled = "true";
+				createEvent(href, targetId, fragment).then((event) => {
+					const actionMethod = Actions.get(action);
+					if (actionMethod) actionMethod(event);
+					link.ariaDisabled = null;
+				});
+			}
+		}
+	});
 }
 //# sourceMappingURL=ayeframe.js.map
--- src/ayeframe.ts
+++ src/ayeframe.ts
@@ -1,45 +1,121 @@
 import { morph } from "morphlex";
 
-type AyeFrameAction = (targetNode: Element | null, content: ChildNode | null | undefined) => void;
+const Actions = new Map<string, (event: Event) => void>();
 
-export function ayeFrame(): void {
-	createFrame("morph", (targetNode, content) => {
-		if (targetNode && content) morph(targetNode, content);
-	});
-
-	createFrame("replace", (targetNode, content) => {
-		if (targetNode && content) targetNode.replaceWith(content);
-	});
-
-	createFrame("append", (targetNode, content) => {
-		if (targetNode && content) targetNode.appendChild(content);
-	});
-
-	createFrame("prepend", (targetNode, content) => {
-		if (targetNode && content) targetNode.prepend(content);
-	});
-
-	createFrame("remove", (targetNode) => {
-		if (targetNode) targetNode.remove();
-	});
+function error(): void {
+	throw new Error("Target or node not found");
 }
 
-function createFrame(name: string, action: AyeFrameAction): void {
-	for (const frame of document.querySelectorAll("iframe")) frame.name === name && frame.remove();
+Actions.set("morph", ({ target, node }) => {
+	if (target && node) {
+		morph(target, node);
+	} else {
+		error();
+	}
+});
 
-	const frame = document.createElement("iframe");
+Actions.set("replace", ({ target, node }) => {
+	if (target && node) {
+		target.replaceWith(node);
+	} else {
+		error();
+	}
+});
 
-	frame.name = name;
-	frame.hidden = true;
-	frame.loading = "lazy";
-	frame.sandbox.add("allow-same-origin");
+Actions.set("append", ({ target, node }) => {
+	if (target && node) {
+		target.appendChild(node);
+	} else {
+		error();
+	}
+});
 
-	frame.addEventListener("load", () => {
-		const id = frame.contentWindow?.location?.hash;
-		if (id) {
-			action(document.querySelector(id) as Element | null, frame.contentDocument?.body?.firstChild);
+Actions.set("prepend", ({ target, node }) => {
+	if (target && node) {
+		target.prepend(node);
+	} else {
+		error();
+	}
+});
+
+Actions.set("refresh", () => {
+	fetch(document.location.href).then((response) => {
+		if (response.ok) {
+			response.text().then((text) => {
+				morph(document.documentElement, text);
+			});
 		}
 	});
+});
 
-	document.body.appendChild(frame);
+Actions.set("remove", ({ target }) => {
+	target?.remove();
+});
+
+Actions.set("navigate", ({ node }) => {
+	if (node) morph(document.documentElement, node);
+});
+
+interface Event {
+	readonly node: Element | null;
+	readonly target: HTMLElement | null;
+}
+
+async function createEvent(href: string, targetId: string | undefined, fragment: string | undefined): Promise<Event> {
+	const headers = new Headers();
+	if (fragment) headers.append("X-Fragment", fragment);
+
+	const event = await fetch(href, { headers }).then(async (response): Promise<Event> => {
+		return await response.text().then((text): Event => {
+			const template = document.createElement("template");
+			template.innerHTML = text;
+			const node = template.content.firstElementChild;
+			const target = targetId ? document.getElementById(targetId) : null;
+
+			return {
+				target,
+				node,
+			};
+		});
+	});
+
+	return event;
+}
+
+export function ayeFrame(): void {
+	// document.addEventListener("submit", (event) => {
+	// 	if (event.target instanceof HTMLFormElement) {
+	// 		console.log("submitted a form");
+	// 	}
+	// });
+
+	document.addEventListener("click", (event) => {
+		const link = event.target;
+
+		if (link instanceof HTMLAnchorElement) {
+			if (event.metaKey || event.ctrlKey || event.shiftKey) return;
+
+			if (link.ariaDisabled) {
+				event.preventDefault();
+				return;
+			}
+
+			const href = link.href;
+			const action = link.dataset.action;
+			const targetId = link.dataset.target;
+			const fragment = link.dataset.fragment;
+
+			if (action) {
+				event.preventDefault();
+				link.ariaDisabled = "true";
+
+				createEvent(href, targetId, fragment).then((event) => {
+					const actionMethod = Actions.get(action);
+					if (actionMethod) actionMethod(event);
+
+					link.ariaDisabled = null;
+				});
+			}
+		}
+	});
 }
